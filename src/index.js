e7bd8e476276002feb02764258f1a4c8f844a375
Olena Jerdeva <lena.jerdeva28@gmail.com>
2024-09-22 14:27:01 +0300

    background-color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import { HashRouter } from 'react-router-dom'; // Используем HashRouter только здесь
+import { HashRouter } from 'react-router-dom'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
